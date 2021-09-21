b834a0ccd0f2d3d3c6ba1a37861e1ee5842e1afc
Maria Mireles <mireles335@gmail.com>
2021-09-20 21:35:21 -0500

    added about test

---
+++ src/components/About/__test__/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    it('renders', () => {
+        render(<About />)
+    })
+    it('matches snapshot DOM node structure', () => {
+       const {asFragment} = render(<About />)
+       expect(asFragment()).toMatchSnapshot();
+    })
+})
