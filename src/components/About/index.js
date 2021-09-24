a205ed6e57d0d090f349078c6556b33064bcc0a6
Maria Mireles <mireles335@gmail.com>
2021-09-24 16:17:27 -0500

    added profolio object

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,8 +4,18 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">Welcome to my page!</h1>
+      <p>
+        {" "}
+        Hi, welcome to my page! Let me introduce myself, my name is Maria
+        Mireles and I am a Austin-based Full Stack Web-Developer. I'm currently
+        remotly attending a coding boot-camp at the University of Texas at
+        Austin. At this moment in time, have a set of skills on HTML, CSS,
+        JavaScript and currently learning Third-Party Web-APIs. Front-End skills
+        aren't just it, I am working on perfecting Back-End skills with
+        technolgies like: Node.JS, SQL{" "}
+      </p>
     </section>
   );
 }
 
-export default About; 
+export default About;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,20 +1,29 @@
 import React from "react";
-import photo from '../../assets/projects/libro-fy.jpg'
-function Portfolio() {
-    
+import photo from "../../assets/projects/libro-fy.jpg";
+
+function Portfolio(props) {
+  const project = {
+    name: "Project Name",
+    description:
+      "Recent projects with respective github and deployment links.",
+  };
   return (
-    <div className="card">
-      <div className="image-container">
-        <img />
+    <section>
+      <div className="flip-card">
+        <div className="image-container">
+          <div class="flip-card-front">
+            <img src={photo} alt="libro" />
+          </div>
+          <div class="flip-card-back">
+            <h1>{project.name}</h1>
+            <p>{project.description}</p>
+          </div>
+        </div>
       </div>
-      <div className="content">
-        <img src={photo}
-        alt="libro"
-        className="img-thumbnail mx-1"
-     />
-      </div>
-    </div>
+    </section>
   );
 }
 
 export default Portfolio;
+
+//material UI
