2e67c5399e546c7ca67938ddcf2bd466b65c421f
Maria Mireles <mireles335@gmail.com>
2021-09-30 20:55:53 -0500

    got photos to render

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -19,9 +19,7 @@ function Nav(props) {
             href={'#' + tab.toLowerCase()}
             onClick={() => props.handlePageChange(tab)}
             className={
-              props.currentPage === tab ? 'nav-link active' : 'nav-link'
-            }
-          >
+              props.currentPage === tab ? 'nav-link active' : 'nav-link'}>
             {tab}
           </a>
         </li>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,70 +1,70 @@
 import React from "react";
-import Card from "@mui/material/Card";
+// import Card from "@mui/material/Card";
 // import CardActions from "@mui/material/CardActions";
 // import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+// import CardMedia from "@mui/material/CardMedia";
 // import Button from "@mui/material/Button";
 // import Typography from "@mui/material/Typography";
 // import PhotoList from "../PhotoList";
+// import photo from "../../assets/challenge/0.jpg"
 
 function Portfolio() {
   const projects = [
     {
-      name: "libroFy",
-      src: "../../assets/challenge/0.jpg",
+      name: "Libro-Fy",
+      src: require("../../assets/challenge/0.jpg").default,
       github: "https://github.com/mariamv29/libro-fy.git",
       deployment: "https://infinite-ravine-50293.herokuapp.com/",
     },
     {
       name: "healthyCraves",
-      src: "../../assets/challenge/1.jpg",
+      src: require("../../assets/challenge/1.jpg").default,
       github: "https://github.com/mariamv29/healthy-craves.git",
       deployment: "https://mariamv29.github.io/healthy-craves/",
     },
     {
       name: " pizzaHut",
-      src: "../../assets/challenge/2.jpg",
+      src: require("../../assets/challenge/2.jpg").default,
       github: "https://github.com/mariamv29/libro-fy.git",
       deployment: "https://obscure-hamlet-00557.herokuapp.com/",
     },
     {
       name: "budgetTracker ",
-      src: "../../assets/challenge/3.jpg",
+      src: require("../../assets/challenge/3.jpg").default,
       github: "https://github.com/mariamv29/libro-fy.git",
       deployment: "https://rocky-forest-04357.herokuapp.com/",
     },
     {
       name: "gitItDone",
-      src: "../../assets/challenge/4.jpg",
+      src: require("../../assets/challenge/4.jpg").default,
       github: "https://github.com/mariamv29/git-it-done.git",
       deployment: "https://libro-fy.herokuapp.com",
     },
     {
       name: "taskinator ",
-      src: "../../assets/challenge/5.jpg",
+      src: require("../../assets/challenge/5.jpg").default,
       github: "https://github.com/mariamv29/taskinator.git",
       deployment: "https://mariamv29.github.io/taskinator/",
     },
   ];
   return (
   
-    
-    <Card sx={{ maxWidth: 345 }}>
-      {projects.map(({src, name }) => (
-        <CardMedia
-          component="img"
-          height="140"
-          image={{src}.default}
-          alt={name}
-          
-        />
-      //   <CardActions>
-      //  <a href={github}</a> 
-      //   <Button size="small">Learn More</Button>
-      // </CardActions>
-      ))}
 
-   </Card>
+    
+  <div>
+      <ul className="navabs">
+      {projects.map(({name, src, github}) => (
+        <li className="navem" key={{name, src, github}}>
+          <a
+            href={github}
+            className="hello">
+          </a>
+          <h1>{name}</h1>
+          <img src={src} alt={name}></img>
+        </li>
+      ))}
+    </ul>
+      </div>
  
   );
 }
